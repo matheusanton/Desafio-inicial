eb5625b22885372c74b88ab50f0f246abbeaa680
matheusanton <rigbymatheus13@gmail.com>
2026-05-02 19:15:00 -0300

    Classificador heroi

---
+++ index.js
@@ -0,0 +1,36 @@
+let nivel = 7500;
+let nome = "MatSx";
+
+if (nivel < 1000){
+    console.log("Ferro");
+
+}else if (nivel >= 1001  &&  nivel < 2000){
+    nivel = " bronze";
+    console.log("A heroina "  + nome + "  esta no nv" +  nivel );
+
+}else if (nivel >= 2001 && nivel  <5000){
+    nivel = " Prata" 
+    console.log("O heroi "  + nome + "  esta no nv" +  nivel);
+
+}else if (nivel >= 5001 && nivel  < 7000){
+    nivel = " Ouro" 
+    console.log("O heroi "  + nome + "  esta no nv" +  nivel);
+
+}else if (nivel >= 7001 && nivel < 8000){
+    nivel = " Platina" 
+    console.log("O heroi "  + nome + "  esta no nv" +  nivel);
+
+}else if (nivel >= 8001 && nivel  <  9000){
+    nivel = " Ascendente" 
+    console.log("O heroi "  + nome + "  esta no nv" +  nivel);
+
+}else if (nivel >= 9001 && nivel < 10000){
+    nivel = " Imortal" 
+    console.log("O heroi "  + nome + "  esta no nv" +  nivel);
+
+}else if ( nivel >= 10001){
+    nivel = " Radiante" 
+    console.log("O heroi "  + nome + "  esta no nv" +  nivel);
+    
+}
+
